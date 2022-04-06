eea43f76961deae0608412504467e5cf097ebdaf
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-04-06 17:48:47 +0800

    mpsc channel

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
+use std::sync::mpsc;
 use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
             workers.push(Worker::new(id));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -32,3 +35,5 @@ impl Worker {
         Worker { id, thread }
     }
 }
+
+struct Job;
