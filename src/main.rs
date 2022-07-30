b952a78649fcb333b7c14f730e0ad1624b3ee131
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-07-31 00:31:50 +0800

    async switch

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,33 @@
-use std::fs;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
+use async_std::{
+    net::{TcpListener, TcpStream},
+    prelude::*,
+    task::{self, spawn},
 };
-use std::thread;
+use futures::StreamExt;
+use std::fs;
 use std::time::Duration;
-use webserver::ThreadPool;
 
-fn main() {
+#[async_std::main]
+async fn main() {
     let opt = Opt::from_args();
     let addr = format!("{}:{}", opt.host, opt.port);
 
-    let listener = TcpListener::bind(addr).unwrap();
+    let listener = TcpListener::bind(addr).await.unwrap();
     let local_addr = listener.local_addr().unwrap();
     println!("Serving on {}", local_addr);
 
-    let shutdown_signal = Arc::new(AtomicBool::new(false));
-    let shutdown_signal_copy = shutdown_signal.clone();
-
-    let handle = thread::spawn(move || {
-        let pool = ThreadPool::new(4);
-        for stream in listener.incoming() {
-            if shutdown_signal_copy.load(Ordering::Relaxed) {
-                break;
-            }
-            match stream {
-                Ok(stream) => {
-                    pool.execute(|| handle_connection(stream));
+    spawn(async move {
+        listener
+            .incoming()
+            .for_each_concurrent(None, |stream| async move {
+                if let Ok(stream) = stream {
+                    // handle_connection(stream).await;
+                    spawn(handle_connection(stream));
                 }
-                Err(e) => {
-                    println!("Error: {:#?}", e);
-                }
-            }
-        }
-    });
-
-    ctrlc::set_handler(move || {
-        println!("Shutting down...");
-        shutdown_signal.store(true, Ordering::Relaxed);
-
-        let mut addr = format!("{}:{}", "127.0.0.1", local_addr.port());
-        if local_addr.is_ipv6() {
-            addr = format!("{}:{}", "[::1]", local_addr.port());
-        }
-        let _ = TcpStream::connect(addr);
+            })
+            .await;
     })
-    .expect("Error setting Ctrl-C handler");
-
-    handle.join().unwrap();
+    .await;
 }
 
 use structopt::StructOpt;
@@ -57,16 +35,16 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "webserver", about = "A webserver demo.")]
 struct Opt {
-    #[structopt(short, long, default_value = "127.0.0.1")]
+    #[structopt(short = "s", long, default_value = "127.0.0.1")]
     host: String,
 
     #[structopt(short, long, default_value = "7878")]
     port: u16,
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    let _ = stream.read(&mut buffer).unwrap();
+    let _ = stream.read(&mut buffer).await.unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -74,7 +52,7 @@ fn handle_connection(mut stream: TcpStream) {
     let (status_line, file_name) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "hello.html")
     } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
+        task::sleep(Duration::from_secs(5)).await;
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -87,6 +65,6 @@ fn handle_connection(mut stream: TcpStream) {
         contents.len(),
         contents,
     );
-    let _ = stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let _ = stream.write(response.as_bytes()).await.unwrap();
+    stream.flush().await.unwrap();
 }
